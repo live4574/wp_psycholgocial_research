da09600e4d7b891740769c80c316a4dac8277799
live4574 <adolfd@naver.com>
2018-12-29 19:38:29 +0900

    hook functio added

--- wp-content/plugins/research/research.php
+++ wp-content/plugins/research/research.php
@@ -1,2 +1,10 @@
-<?php /* Plugin Name: research Description: research psychological for WordPress Version: 1.0 Author: lee  License: GPLv2 or later */ ?>
+<?php /* Plugin Name: research Description: research psychological for WordPress Version: 1.0 Author: lee  License: GPLv2 or later */ 
+
+function fwds_slider_activation() {
+}
+register_activation_hook(__FILE__, 'fwds_slider_activation');
+function fwds_slider_deactivation() {
+}
+register_deactivation_hook(__FILE__, 'fwds_slider_deactivation');
+?>
 
