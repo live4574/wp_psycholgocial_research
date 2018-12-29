381fde8f7434286b49d99d7c3134941f4340935f
live4574 <adolfd@naver.com>
2018-12-29 19:32:35 +0900

    plugin remark

---
+++ wp-content/plugins/research/research.php
@@ -0,0 +1,3 @@
+<?php /* Plugin Name: research Description: research psychological for WordPress Version: 1.0 Author: lee  License: GPLv2 or later */ ?>
+
+php/* Plugin Name: reseach Description: research psychological for WordPress Version: 1.0 Author: lee License: GPLv2 or later */ ?>
