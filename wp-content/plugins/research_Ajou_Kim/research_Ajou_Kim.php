778f02df1657bad352ea62bfeb3d2658bb610371
live4574 <adolfd@naver.com>
2019-02-03 03:14:26 +0900

    database admin update test

--- wp-content/plugins/research_Ajou_Kim/research_Ajou_Kim.php
+++ wp-content/plugins/research_Ajou_Kim/research_Ajou_Kim.php
@@ -8,17 +8,38 @@ Author: Lee
 Author URI: https://github.com/live4574 
 License: GPL2 
  */ 
-
-add_action('admin_menu', 'my_plugin_menu');
-function my_plugin_menu() {
-   add_options_page('My Plugin Options', 'My Plugin', 'manage_options', 'my-unique-identifier', 'my_plugin_options');
-}
-function my_plugin_options() {
-   if (!current_user_can('manage_options'))
+add_action('admin_menu', 'mt_add_pages');
+// 훅에 대한 액션 함수
+ function mt_add_pages() {
+ // Settings 메뉴에 서브메뉴를 만듬.
+ add_options_page('Test Settings', 'Test Settings', 'manage_options', 'testsettings', 'mt_settings_page');
+ }
+function mt_settings_page() {
+ // 유저 접근 권한 체크
+ if (!current_user_can('manage_options')) 
      wp_die( __('You do not have sufficient permissions to access this page.') );
-?>
-   <div class='wrap'>
-   <h2>My Plugin Options</h2>
-   <p>Here is where the form would go if I actually had options.</p>
-   </div>
-<? } ?>
+// 필드와 옵션 이름으로 사용할 변수
+ $opt_name = 'mt_favorite_color';
+ $hidden_field_name = 'mt_submit_hidden';
+ $data_field_name = 'mt_favorite_color';
+// 현재 데이터베이스에 저장된 옵션 값 가져오기
+ $opt_val = get_option( $opt_name );
+// Form을 Submit 했다면...
+ if( isset($_POST[ $hidden_field_name ]) && $_POST[ $hidden_field_name ] == 'Y' ) {
+ $opt_val = $_POST[ $data_field_name ];
+// 포스트값을 데이터베이스에 저장
+ update_option( $opt_name, $opt_val );
+// 화면에 업데이트했다는 메세지를 표시
+ echo '<div><p><strong>settings saved</p></strong></div>';
+ }
+ // 옵션 설정 화면을 표시
+ ?>
+ <div class='wrap'>
+ <h2>Menu Test Plugin Settings</h2>
+ <form method="post" action="">
+ <input value="Y">
+ <p>Favorite Color: <input value="<?=$opt_val?>"></p>
+ <p><input value="Save Changes" /></p>
+ </form>
+ </div>
+ <?php } ?>
