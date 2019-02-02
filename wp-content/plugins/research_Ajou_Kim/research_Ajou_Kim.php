9ae48cc66414cf0de2e4666fc70ba1241ddbbbd5
live4574 <adolfd@naver.com>
2019-02-03 03:11:07 +0900

    my plugin option test

--- wp-content/plugins/research_Ajou_Kim/research_Ajou_Kim.php
+++ wp-content/plugins/research_Ajou_Kim/research_Ajou_Kim.php
@@ -1,27 +1,24 @@
+<?php
+/* 
+Plugin Name: Research Ajou 
+Plugin URI:https://github.comlive4574/wp_psycholgocial_research 
+Description: A research. 
+Version: 1.0 
+Author: Lee 
+Author URI: https://github.com/live4574 
+License: GPL2 
+ */ 
 
-<?php
-/*
-Plugin Name: Research Ajou
-Plugin URI: https://github.com/live4574/wp_psycholgocial_research
-Description: A research.
-Version: 1.0
-Author: Lee
-Author URI: https://github.com/live4574
-License: GPL2
-*/
-<?php
-add_action('admin_menu','my_plugin_menu');
-function my_plugin_menu(){
-	add_options_page('My Plugin Options', 'My Plugin', 'manage_options','my-unique-identifier','my_plugin_options');
+add_action('admin_menu', 'my_plugin_menu');
+function my_plugin_menu() {
+   add_options_page('My Plugin Options', 'My Plugin', 'manage_options', 'my-unique-identifier', 'my_plugin_options');
 }
-function my_plugin_options(){
-	if(!current_user_can('manage_options'))
-	wp_die(__('U do not have sufficient permissions to acess this page.'));
+function my_plugin_options() {
+   if (!current_user_can('manage_options'))
+     wp_die( __('You do not have sufficient permissions to access this page.') );
 ?>
-<div class='wrap'>
-<h2>My plugin options</h2>
-<p> Here is where the form would go if I actually had options.</p>
-</div>
-<?}?>
-
-?>
+   <div class='wrap'>
+   <h2>My Plugin Options</h2>
+   <p>Here is where the form would go if I actually had options.</p>
+   </div>
+<? } ?>
