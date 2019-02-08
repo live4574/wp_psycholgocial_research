56f2aa6846a64047b91ca0ab566321c352d2f347
living4574 <adolfd@naver.com>
2019-02-08 20:07:18 +0900

    magic function triggered on initialization remark

--- wp-content/plugins/research_Ajou_Kim/wp_research_Ajou_Kim_Plugin.php
+++ wp-content/plugins/research_Ajou_Kim/wp_research_Ajou_Kim_Plugin.php
@@ -23,14 +23,16 @@ class wp_simple_location{
 	//include widgets
 }
 publci function __construct(){
-	add_action('init', array($this,'set_location_survey_hour_days')); 
-    add_action('init', array($this,'register_location_content_type')); 
-    add_action('add_meta_boxes', array($this,'add_location_meta_boxes')); 
-    add_action('save_post_wp_locations', array($this,'save_location')); 
-    add_action('admin_enqueue_scripts', array($this,'enqueue_admin_scripts_and_styles')); 
-    add_action('wp_enqueue_scripts', array($this,'enqueue_public_scripts_and_styles')); 
+	add_action('init', array($this,'set_location_survey_hour_days')); //set the default survey hour days(used by the content type)
+    add_action('init', array($this,'register_location_content_type')); //register location content type
+    add_action('add_meta_boxes', array($this,'add_location_meta_boxes')); //add meta boexs
+    add_action('save_post_wp_locations', array($this,'save_location'));  //save location
+    add_action('admin_enqueue_scripts', array($this,'enqueue_admin_scripts_and_styles')); //admin scripts and styles
+    add_action('wp_enqueue_scripts', array($this,'enqueue_public_scripts_and_styles'));
+    //public scripts and styles 
     add_filter('the_content', array($this,'prepend_location_meta_to_content')); 
-    register_activation_hook(__FILE__, array($this,'plugin_activate')); 
-    register_deactivation_hook(__FILE__, array($this,'plugin_deactivate'));
+    //gets our meta data and dispayed it before the content
+    register_activation_hook(__FILE__, array($this,'plugin_activate')); //activate hook
+    register_deactivation_hook(__FILE__, array($this,'plugin_deactivate')); //deactivate hook
 }
 //magic function triggered on initialization
