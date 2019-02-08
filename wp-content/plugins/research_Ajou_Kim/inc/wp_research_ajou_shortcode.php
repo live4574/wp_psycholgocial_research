ee4085d2c5ae97bf7642ae23334d6016a9349eb6
lee <adolfd@naver.com>
2019-02-08 22:36:23 +0900

    short code

---
+++ wp-content/plugins/research_Ajou_Kim/inc/wp_research_ajou_shortcode.php
@@ -0,0 +1,37 @@
+<?php
+defined('ABSPATH') or die('Nope, not accessing this');
+
+class wp_survey_shortcode{
+}
+//defines the functionality for the survey shortcode
+
+public function __construct(){
+	add_action('init', array($this,'register_survey_shortcodes'));
+	//shortcodes
+}
+//on initialize
+
+public function register_survey_shorcodes(){
+	add_shortcode('wp_surveys',array($this,'survey_shortcode_output'));
+}
+
+//shortcode display
+public function survey_shortcode_output($atts, $content = '', $tag){
+
+    //get the global wp_simple_surveys class
+    global $wp_simple_surveys;
+
+    //build default arguments
+    $arguments = shortcode_atts(array(
+        'survey_id' => '',
+        'number_of_surveys' => -1)
+    ,$atts,$tag);
+
+    //uses the main output function of the survey class
+    $html = $wp_simple_surveys->get_surveys_output($arguments);
+
+    return $html;
+}
+
+$wp_survey_shortcode=new wp_survey_shortcode;
+?>
