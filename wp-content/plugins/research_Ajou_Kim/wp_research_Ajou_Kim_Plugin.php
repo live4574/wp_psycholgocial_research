fb230714a530f2ad65bc2d212380970261594cc5
living4574 <adolfd@naver.com>
2019-02-08 20:21:25 +0900

    survey content type function

--- wp-content/plugins/research_Ajou_Kim/wp_research_Ajou_Kim_Plugin.php
+++ wp-content/plugins/research_Ajou_Kim/wp_research_Ajou_Kim_Plugin.php
@@ -16,48 +16,48 @@ License: GPL2
 private $wp_ajou_survey_seconds=array();
 //properties
 
-class wp_simple_location{
+class wp_simple_survey{
 	include(plugin_dir_path(__FILE__) . 'inc/wp_research_ajou_shortcode.php');
 	//include shortcodes
 	include(plugin_dir_path(__FILE__) . 'inc/wp_research_ajou_widget.php');
 	//include widgets
 }
 publci function __construct(){
-	add_action('init', array($this,'set_location_survey_hour_days')); //set the default survey hour days(used by the content type)
-    add_action('init', array($this,'register_location_content_type')); //register location content type
+	add_action('init', array($this,'set_survey_hour_days')); //set the default survey hour days(used by the content type)
+    add_action('init', array($this,'register_survey_content_type')); //register location content type
     add_action('add_meta_boxes', array($this,'add_location_meta_boxes')); //add meta boexs
-    add_action('save_post_wp_locations', array($this,'save_location'));  //save location
+    add_action('save_post_wp_locations', array($this,'save_survey'));  //save location
     add_action('admin_enqueue_scripts', array($this,'enqueue_admin_scripts_and_styles')); //admin scripts and styles
     add_action('wp_enqueue_scripts', array($this,'enqueue_public_scripts_and_styles'));
     //public scripts and styles 
-    add_filter('the_content', array($this,'prepend_location_meta_to_content')); 
+    add_filter('the_content', array($this,'prepend_survey_meta_to_content')); 
     //gets our meta data and dispayed it before the content
     register_activation_hook(__FILE__, array($this,'plugin_activate')); //activate hook
     register_deactivation_hook(__FILE__, array($this,'plugin_deactivate')); //deactivate hook
 }
 //magic function triggered on initialization
 
-public function set_location_survey_hour_days(){
-	$this->wp_location_survey_hour_days=apply_filtres('wp_location_survey_hours_days',array('monday'=>'Monday','tuesday'=>'Tuesday','wednesday'=>'Wednesday'  'thursday' => 'Thursday','friday' => 'Friday','saturday' => 'Saturday','sunday' => 'Sunday',
+public function set_survey_hour_days(){
+	$this->wp_survey_hour_days=apply_filtres('wp_location_survey_hours_days',array('monday'=>'Monday','tuesday'=>'Tuesday','wednesday'=>'Wednesday'  'thursday' => 'Thursday','friday' => 'Friday','saturday' => 'Saturday','sunday' => 'Sunday',
         ));
 }
 //set the default survey hour days(used in admin backend)
 
-public function register_location_content_type(){
-	$labels = array( 'name'               => 'Location',
-           'singular_name'      => 'Location',
-           'menu_name'          => 'Locations',
-           'name_admin_bar'     => 'Location',
+public function register_survey_content_type(){
+	$labels = array( 'name'               => 'Survey_Ajou',
+           'singular_name'      => 'Survey_Ajou',
+           'menu_name'          => 'Survey_Ajou',
+           'name_admin_bar'     => 'Survey_Ajou',
            'add_new'            => 'Add New', 
-           'add_new_item'       => 'Add New Location',
-           'new_item'           => 'New Location', 
-           'edit_item'          => 'Edit Location',
-           'view_item'          => 'View Location',
-           'all_items'          => 'All Locations',
-           'search_items'       => 'Search Locations',
-           'parent_item_colon'  => 'Parent Location:', 
-           'not_found'          => 'No Locations found.', 
-           'not_found_in_trash' => 'No Locations found in Trash.',
+           'add_new_item'       => 'Add New Survey',
+           'new_item'           => 'New Survey', 
+           'edit_item'          => 'Edit Survey',
+           'view_item'          => 'View Survey',
+           'all_items'          => 'All Surveys',
+           'search_items'       => 'Search Surveys',
+           'parent_item_colon'  => 'Parent Surveys:', 
+           'not_found'          => 'No Surveys found.', 
+           'not_found_in_trash' => 'No Surveys found in Trash.',
        );
 	//labels for post type
 	$args=array(
@@ -73,9 +73,9 @@ public function register_location_content_type(){
            'menu_position'     => 20,
            'show_in_admin_bar' => true,
            'menu_icon'         => 'dashicons-location-alt',
-           'rewrite'            => array('slug' => 'locations', 'with_front' => 'true')
+           'rewrite'            => array('slug' => 'surveys', 'with_front' => 'true')
        );
 	//argument for post type
-	register_post_type('wp_locations',$args);
+	register_post_type('wp_surveys',$args);
 	//register post type
 }
