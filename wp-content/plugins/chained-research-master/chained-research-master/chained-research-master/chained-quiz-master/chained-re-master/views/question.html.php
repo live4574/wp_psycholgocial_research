e64f6bdfe182d4d4e06988b32184ba59b79e2d51
living4574 <adolfd@naver.com>
2019-02-14 17:48:10 +0900

    qtype none added to view-question

--- wp-content/plugins/chained-research-master/chained-research-master/chained-research-master/chained-quiz-master/chained-re-master/views/question.html.php
+++ wp-content/plugins/chained-research-master/chained-research-master/chained-research-master/chained-quiz-master/chained-re-master/views/question.html.php
@@ -10,6 +10,7 @@
 		<p><label><?php _e('Question title', 'chained')?></label> <input type="text" name="title" size="40" value="<?php echo @$question->title?>"></p>
 		<p><label><?php _e('Question contents', 'chained')?></label> <?php echo wp_editor(stripslashes(@$question->question), 'question')?></p>
 		<p><label><?php _e('Question type:', 'chained')?></label> <select name="qtype" onchange="this.value == 'radio' ? jQuery('#chainedAutoContinue').show() : jQuery('#chainedAutoContinue').hide();">
+			<option value="none" <?php if(!empty($question->id) and $question->qtype == 'none') echo 'selected'?>><?php _e('None','chained')?></option>
 			<option value="radio" <?php if(!empty($question->id) and $question->qtype == 'radio') echo 'selected'?>><?php _e('Radio buttons (one possible answer)','chained')?></option>
 			<option value="checkbox" <?php if(!empty($question->id) and $question->qtype == 'checkbox') echo 'selected'?>><?php _e('Checkboxes (multiple possible answers)','chained')?></option>
 			<option value="text" <?php if(!empty($question->id) and $question->qtype == 'text') echo 'selected'?>><?php _e('Text box (open-end, essay question)','chained')?></option>
