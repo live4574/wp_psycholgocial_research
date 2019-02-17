b912f58927a94de51925c6eb9384aa86e94c3a98
living4574 <adolfd@naver.com>
2019-02-17 16:32:39 +0900

    directory modified -too long

--- wp-content/plugins/wp-quizr-master/wp-quizr-master/admin/partials/wp-quizr-display-social-ids-reminder.php
+++
@@ -1,7 +0,0 @@
-
-<div class="inside">
-    
-    <p class="metabox-p"><?php esc_html_e('Don\'t forget to provide at least one social network to which you would like users to share their quiz results. Go to WP Quizr Settings to do this.', 'wp-quizr'); ?></p>
-
-</div>
-
