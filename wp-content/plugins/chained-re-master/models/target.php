af07ac38b596901e307e1d647cd48f1af316672d
live4574 <adolfd@naver.com>
2019-02-20 02:16:28 +0900

    model target name renamed to CHAINED_TARGETS

--- wp-content/plugins/chained-re-master/models/target.php
+++ wp-content/plugins/chained-re-master/models/target.php
@@ -2,19 +2,19 @@
 class ChainedQuizTarget{
 	function add($vars){
 		global $wpdb;
-		$target=$wpdb->query($wpdb->prepare("INSERT INTO ".CHAINED_QUIZ_TARGETS." SET question_id=$d, target_id=$d, target_name=$s",$vars['question_id'],$vars['target_id'],$vars['target_name'] ));
+		$target=$wpdb->query($wpdb->prepare("INSERT INTO ".CHAINED_TARGETS." SET question_id=$d, target_id=$d, target_name=$s",$vars['question_id'],$vars['target_id'],$vars['target_name'] ));
 		if($target==false) thow new Exception(__('DB Error'),'chained'));
 		return $wpdb->insert_id;
 	}
 	function save($vars,$id){
 		global $wpdb;
-		$target=$wpdb->query($wpdb->prepare("UPDATE ".CHAINED_QUIZ_TARGETS." SET question_id=$d, target_id=$d, target_name=$s"),$vars['question_id'],$vars['target_id'],$vars['target_name']),$id);
+		$target=$wpdb->query($wpdb->prepare("UPDATE ".CHAINED_TARGETS." SET question_id=$d, target_id=$d, target_name=$s"),$vars['question_id'],$vars['target_id'],$vars['target_name']),$id);
 		if($target==false) throw new Exception(__('DB Error'), 'chained'));
 		return true;
 	}
 	function delete($id){
 		global $wpdb;
-		$target=$wpdb=>query($wpdb->prepare("DELETE FROM ".CHAINED_QUIZ_TARGETS." WHERE id=%d", $id));
+		$target=$wpdb=>query($wpdb->prepare("DELETE FROM ".CHAINED_TARGETS." WHERE id=%d", $id));
 		if($result==false) throw new Exception(__'DB Error', 'chained'));
 		return true;
 	}
