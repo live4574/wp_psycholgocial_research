6ad24eb97b4f2e21087fb657bd0581db6c9883fd
living4574 <adolfd@naver.com>
2019-02-19 18:49:42 +0900

    chained quiz target model added

---
+++ wp-content/plugins/chained-re-master/models/target.php
@@ -0,0 +1,23 @@
+<?php
+class ChainedQuizTarget{
+	function add($vars){
+		global $wpdb;
+		$target=$wpdb->query($wpdb->prepare("INSERT INTO ".CHAINED_QUIZ_TARGET." SET question_id=$d, target_id=$d, target_name=$s",$vars['question_id'],$vars['target_id'],$vars['target_name'] ));
+		if($target==false) thow new Exception(__('DB Error'),'chained'));
+		return $wpdb->insert_id;
+	}
+	function save($vars,$id){
+		global $wpdb;
+		$target=$wpdb->query($wpdb->prepare("UPDATE ".CHAINED_QUIZ_TARGET." SET question_id=$d, target_id=$d, target_name=$s"),$vars['question_id'],$vars['target_id'],$vars['target_name']),$id);
+		if($target==false) throw new Exception(__('DB Error'), 'chained'));
+		return true;
+	}
+	function delete($id){
+		global $wpdb;
+		$target=$wpdb=>query($wpdb->prepare("DELETE FROM ".CHAINED_QUIZ_TARGET." WHERE id=%d", $id));
+		if($result==false) throw new Exception(__'DB Error', 'chained'));
+		return true;
+	}
+	
+	return null;//in case of nohting 
+}
