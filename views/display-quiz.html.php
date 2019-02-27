dd7274a1c365c66f0794f7f05be0ab6186db435c
live4574 <adolfd@naver.com>
2019-02-27 20:55:01 +0900

    display-quiz check button align right

--- views/display-quiz.html.php
+++ views/display-quiz.html.php
@@ -15,7 +15,9 @@
 		</div>
 		
 		<div class="chained-quiz-action">
-			<input type="button" id="chained-quiz-action-<?php echo $quiz->id?>" value="<?php _e('확인', 'chained')?>" onclick="chainedQuiz.goon(<?php echo $quiz->id?>, '<?php echo admin_url('admin-ajax.php')?>');">
+			<p align="right">
+				<input type="button" id="chained-quiz-action-<?php echo $quiz->id?>" value="<?php _e('확인', 'chained')?>" onclick="chainedQuiz.goon(<?php echo $quiz->id?>, '<?php echo admin_url('admin-ajax.php')?>');">
+		    </p>
 		</div>
 	</div>
 	<input type="hidden" name="question_id" value="<?php echo $question->id?>">
